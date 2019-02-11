a49928ed3e2015b6c5a4c7d8278fc0ed3fd24bc6
Adam Moran <jeffadammoran@gmail.com>
2019-02-11 22:23:55 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,41 @@
+require 'pry'
 # Write your code here.
+
+def dictionary (tweet_array)
+  dictionary_hash = {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&",
+  }
+  
+  new_array = []
+  
+  new_tweet_array = 
+  tweet_array.collect do |i|
+    new_word = i
+    keys = dictionary_hash.keys
+    #binding.pry
+    keys.each do |j|
+      if j == i {new_word = dictionary_hash[j]}
+      #end
+    end
+    i = new_word
+  end
+  new_tweet_array
+end
+
+def word_substituter (tweet)
+  split_tweet = tweet.split(" ")
+  fixed_split_tweet = dictionary (split_tweet)
+  joined_tweet = fixed_split_tweet.join(" ") 
+  joined_tweet
+end
+  
+  
